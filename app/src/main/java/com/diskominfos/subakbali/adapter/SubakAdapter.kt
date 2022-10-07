b36070fd142538781d1302de98f57b96877e89a5
Shilta Inda <shiltainda@gmail.com>
2022-10-08 06:37:31 +0800

    fix recycler subak

---
+++ app/src/main/java/com/diskominfos/subakbali/adapter/SubakAdapter.kt
@@ -0,0 +1,67 @@
+package com.diskominfos.subakbali.adapter
+
+import android.app.Activity
+import android.content.ClipData.Item
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.diskominfos.subakbali.api.DataSubak
+import com.diskominfos.subakbali.databinding.ItemSubakBinding
+
+class SubakAdapter(private val subakList: MutableList<DataSubak>) :
+    RecyclerView.Adapter<SubakAdapter.SubakViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    inner class SubakViewHolder(private val binding: ItemSubakBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(subak: DataSubak) {
+            binding.root.setOnClickListener {
+                onItemClickCallback.onItemClicked(subak)
+            }
+
+            binding.apply {
+//                Glide.with(itemView).load(subak.photoUrl)
+//                    .transition(DrawableTransitionOptions.withCrossFade()).centerCrop()
+//                    .into(photo)
+                nama.text = subak.nama
+                jenis.text = subak.jenis_subak
+
+                itemView.setOnClickListener {
+//                    val intent = Intent(itemView.context, DetailStoryActivity::class.java)
+//                    intent.putExtra("DataSubak", subak)
+                    val optionsCompat: ActivityOptionsCompat =
+                        ActivityOptionsCompat.makeSceneTransitionAnimation(
+                            itemView.context as Activity,
+                            Pair(nama, "nama"),
+                            Pair(jenis, "jenis_subak"),
+                        )
+//                    itemView.context.startActivity(intent, optionsCompat.toBundle())
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubakViewHolder {
+        val view = ItemSubakBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return SubakViewHolder((view))
+    }
+
+    override fun onBindViewHolder(holder: SubakViewHolder, position: Int) {
+        holder.bind(subakList[position])
+    }
+
+    override fun getItemCount(): Int = subakList.size
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: DataSubak)
+    }
+
+    fun setOnClickCallBack(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+}
+
--- app/src/main/java/com/diskominfos/subakbali/ui/home/HomeFragment.kt
+++ app/src/main/java/com/diskominfos/subakbali/ui/home/HomeFragment.kt
@@ -1,20 +1,36 @@
 package com.diskominfos.subakbali.ui.home
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.diskominfos.subakbali.adapter.SubakAdapter
+import com.diskominfos.subakbali.api.DataSubak
 import com.diskominfos.subakbali.databinding.FragmentHomeBinding
+import com.diskominfos.subakbali.model.UserPreference
+import com.diskominfos.subakbali.model.ViewModelFactory
+import com.diskominfos.subakbali.ui.auth.LoginActivity
+import com.diskominfos.subakbali.ui.data.DataViewModel
+import com.diskominfos.subakbali.ui.profil.ProfilViewModel
 
+private lateinit var context: Context
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 class HomeFragment : Fragment() {
-
+    private lateinit var profilViewModel: ProfilViewModel
+    private lateinit var dataViewModel: DataViewModel
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,11 +44,60 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+        setupViewModel()
+        setupRecyclerView()
         return root
+    }
+
+    private fun setupRecyclerView() {
+        val layoutManager = LinearLayoutManager(context)
+        binding.rvSubak.layoutManager = layoutManager
+        binding.rvSubak.setHasFixedSize(true)
+    }
+
+    private fun setupViewModel() {
+        val profilViewModel: ProfilViewModel by viewModels {
+            val pref = requireContext().dataStore
+            ViewModelFactory(
+                UserPreference.getInstance(pref),
+                requireContext()
+            )
+        }
+
+        dataViewModel = ViewModelProvider(this)[DataViewModel::class.java]
+        profilViewModel.getUser().observe(viewLifecycleOwner) {
+
+            if (it != "") {
+                dataViewModel.getSubak(it)
+                dataViewModel.subakList.observe(viewLifecycleOwner) { subak ->
+                    getList(subak)
+                    if (it != null) {
+//                        showLoading(false)
+                    }
+                    Log.d("error", "setupViewModel: $subak")
+                }
+            } else {
+                val intent = Intent(activity, LoginActivity::class.java)
+                activity?.startActivity(intent)
+            }
+        }
+    }
+
+    private fun getList(subak: MutableList<DataSubak>) {
+        binding.rvSubak.layoutManager = GridLayoutManager(context, 2)
+        val subakAdapter = SubakAdapter(subak)
+        binding.rvSubak.adapter = subakAdapter
+//        subakAdapter.setOnClickCallBack(object : SubakAdapter.OnItemClickCallback {
+//            override fun onItemClicked(data: DataSubak) {
+//                Intent(this@HomeFragment, DetailStoryActivity::class.java).also {
+//                    it.putExtra(DetailStoryActivity.NAME, data.name)
+//                    it.putExtra(DetailStoryActivity.ID, data.id)
+//                    it.putExtra(DetailStoryActivity.DESCRIPTION, data.description)
+//                    it.putExtra(DetailStoryActivity.PHOTO, data.photoUrl)
+//                    startActivity(it)
+//                }
+//            }
+//        })
     }
 
     override fun onDestroyView() {
