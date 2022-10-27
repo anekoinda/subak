971d802e32a8052813a933b8bc1b51d7dcefa5db
Shilta Inda <shiltainda@gmail.com>
2022-10-27 19:39:07 +0800

    list add data

---
+++ app/src/main/java/com/diskominfos/subakbali/ListData.kt
@@ -0,0 +1,11 @@
+package com.diskominfos.subakbali
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_data)
+    }
+}
