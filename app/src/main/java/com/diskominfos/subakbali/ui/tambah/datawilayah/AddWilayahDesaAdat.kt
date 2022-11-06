104586da67b9549613613d73299f5a97ca74bee2
Shilta Inda <shiltainda@gmail.com>
2022-11-07 00:57:25 +0800

    layout data wilayah DD

--- app/src/main/java/com/diskominfos/subakbali/ui/tambah/datawilayah/AddWilayahDesaAdat.kt
+++ app/src/main/java/com/diskominfos/subakbali/ui/tambah/datawilayah/AddWilayahDesaAdat.kt
@@ -1,7 +1,8 @@
-package com.diskominfos.subakbali
+package com.diskominfos.subakbali.ui.tambah.datawilayah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.diskominfos.subakbali.R
 
 class AddWilayahDesaAdat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/diskominfos/subakbali/ui/tambah/datawilayah/AddWilayahDesaDinas.kt
@@ -0,0 +1,12 @@
+package com.diskominfos.subakbali.ui.tambah.datawilayah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diskominfos.subakbali.R
+
+class AddWilayahDesaDinas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_wilayah_desa_dinas)
+    }
+}
