a67c1a72c4245db2adb4079cfdebd864c4bdc88c
Shilta Inda <shiltainda@gmail.com>
2022-11-08 04:39:50 +0800

    layout sumber dana

---
+++ app/src/main/java/com/diskominfos/subakbali/ui/tambah/sumberdana/AddSumberDana.kt
@@ -0,0 +1,12 @@
+package com.diskominfos.subakbali.ui.tambah.sumberdana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diskominfos.subakbali.R
+
+class AddSumberDana : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_sumber_dana)
+    }
+}
---
+++ app/src/main/java/com/diskominfos/subakbali/ui/tambah/sumberdana/SumberDana.kt
@@ -0,0 +1,12 @@
+package com.diskominfos.subakbali.ui.tambah.sumberdana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diskominfos.subakbali.R
+
+class SumberDana : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sumber_dana)
+    }
+}
