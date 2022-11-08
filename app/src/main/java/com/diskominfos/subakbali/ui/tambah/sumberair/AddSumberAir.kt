350e6a89c13bcb7c015061937527cb42b49c01f1
Shilta Inda <shiltainda@gmail.com>
2022-11-09 00:59:59 +0800

    layout sumber air

---
+++ app/src/main/java/com/diskominfos/subakbali/ui/tambah/sumberair/AddSumberAir.kt
@@ -0,0 +1,12 @@
+package com.diskominfos.subakbali.ui.tambah.sumberair
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diskominfos.subakbali.R
+
+class AddSumberAir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_sumber_air)
+    }
+}
---
+++ app/src/main/java/com/diskominfos/subakbali/ui/tambah/sumberair/SumberAir.kt
@@ -0,0 +1,12 @@
+package com.diskominfos.subakbali.ui.tambah.sumberair
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diskominfos.subakbali.R
+
+class SumberAir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sumber_air)
+    }
+}
